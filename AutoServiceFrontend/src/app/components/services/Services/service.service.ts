d41c0c1798db1d3a8c0c612000fbf69eb9f8d45c
AkramjonA <akramzonabduvahobov6@gmail.com>
2024-05-04 16:14:55 +0500

    add services and Service Categories service

---
+++ AutoServiceFrontend/src/app/components/services/Services/service.service.ts
@@ -0,0 +1,41 @@
+import { UpdtateService } from '../../models/models/ServiceModels/ServiceModels/UpdtateService';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
+import { CreateService } from '../../models/models/ServiceModels/ServiceModels/CreateService';
+import { ResponceModel } from '../../models/models/ResponceModels/responcemodel';
+import { ServiceModel } from '../../models/models/ServiceModels/ServiceModels/ServiceModel';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ServiceService {
+  private apiUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {}
+
+  create(date: CreateService) {
+    return this.http.post<ResponceModel>(
+      `${this.apiUrl}Service/CreateService`,
+      date
+    );
+  }
+
+  update(date: UpdtateService) {
+    return this.http.put<ResponceModel>(
+      `${this.apiUrl}Service/UpdateService`,
+      date
+    );
+  }
+
+  delete(date: string) {
+    return this.http.delete<ResponceModel>(
+      `${this.apiUrl}Service/DeleteService?id=${date}`
+    );
+  }
+
+  getAll(date: number, date2: number) {
+    return this.http.get<ServiceModel[]>(
+      `${this.apiUrl}Service/GetAllService?PageIndex=${date}&Size=${date2}`
+    );
+  }
+}
